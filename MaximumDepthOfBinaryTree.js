405970e496944adac82275ce62f8db0e39b25125
h-miiaah <haim.miiaah5@gmail.com>
2022-01-14 15:44:08 -0500

    comments

--- MaximumDepthOfBinaryTree.js
+++ MaximumDepthOfBinaryTree.js
@@ -24,5 +24,6 @@ var rootFinder = function (root, level) {
 
 - We start with a function that takes in a node and a depth.
 - If the node is null, we return the depth.
+- Otherwise, we recursively call the function with the node’s left and right children and the depth + 1.
 
 */
